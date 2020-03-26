36bf5c1f195d9d33eac924ea21054ee5d13a6725
Victor Untulius <untulius@gmail.com>
2020-03-27 00:37:47 +0300

    Add Lesson17

---
+++ src/test/java/org/example/Lesson17.java
@@ -0,0 +1,147 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/*
+Добавить действия в тест из задания к занятию 16:
+1. На странице “Select” выбрать одно значение в выпадающем списка и несколько в
+списке, поддерживающем множественный выбор.
+2. Нажать на кнопку “GET RESULT”. Проверить, что на странице отобразились выбранные
+значения и ссылка с текстом “Great! Return to menu” и нажать на неё.
+3. На странице “Form” заполнить все обязательные поля и нажать на кнопку
+«ОТПРАВИТЬ».
+4. Проверить, что появилась ссылка с текстом “Great! Return to menu” и нажать на неё.
+5. На странице “IFrame” ввести код, выведенный на этой странице, в поле ввода и нажать
+на кнопку «VERIFY».
+6. Проверить, что появилась ссылка с текстом “Great! Return to menu” и нажать на неё.
+ */
+public class Lesson17 {
+    private static final Logger LOG = LoggerFactory.getLogger(Lesson16.class);
+    private WebDriver webDriver;
+
+    @BeforeClass
+    public void downloadDriver() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    public void initDriver() {
+        webDriver = new ChromeDriver();
+    }
+
+    @Test
+    public void lesson17() {
+        webDriver.get("https://savkk.github.io/selenium-practice/");
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        /*
+        //Lesson16:
+        webDriver.findElement(By.id("button")).click();
+        webDriver.findElement(By.id("first")).click();
+
+        WebElement labelExcellent = webDriver.findElement(By.xpath("//label[.='Excellent!']"));
+        Assert.assertEquals(labelExcellent.getText(), "Excellent!");
+
+        WebElement button = webDriver.findElement(By.className("button-primary"));
+        Assert.assertEquals(button.getAttribute("value"), "Click me too!");
+        button.click();
+
+        WebElement link = webDriver.findElement(By.xpath("//a[.='Great! Return to menu']"));
+        Assert.assertEquals(link.getText(), "Great! Return to menu");
+        link.click();
+
+        webDriver.findElement(By.id("checkbox")).click();
+
+        WebElement checkBoxOne = webDriver.findElement(By.id("one"));
+        checkBoxOne.click();
+        webDriver.findElement(By.id("go")).click();
+        WebElement labelResult = webDriver.findElement(By.id("result"));
+        Assert.assertEquals(labelResult.getText(), checkBoxOne.getAttribute("value"));
+
+        WebElement radioBoxThree = webDriver.findElement(By.id("radio_three"));
+        radioBoxThree.click();
+        webDriver.findElement(By.id("radio_go")).click();
+        WebElement labelRadioResult = webDriver.findElement(By.id("radio_result"));
+        Assert.assertEquals(labelRadioResult.getText(), radioBoxThree.getAttribute("value"));
+
+        WebElement linkReturn = webDriver.findElement(By.xpath("//a[.='Great! Return to menu']"));
+        Assert.assertEquals(linkReturn.getText(), "Great! Return to menu");
+        linkReturn.click();
+
+
+        //Lesson17:
+        webDriver.findElement(By.id("select")).click();
+        WebElement heroElement  = webDriver.findElement(By.name("hero"));
+        Select heroSelect = new Select(heroElement);
+        heroSelect.selectByVisibleText("Frederick Phillips Brooks, Jr.");
+
+        WebElement languagesElement = webDriver.findElement(By.name("languages"));
+        Select languagesSelect = new Select(languagesElement);
+        languagesSelect.selectByValue("Java");
+        languagesSelect.selectByValue("C++");
+        languagesSelect.selectByValue("Pascal");
+
+        webDriver.findElement(By.id("go")).click();
+        WebElement languagesResult = webDriver.findElement(By.xpath("//select[@name='hero']/following-sibling::label"));
+        Assert.assertEquals(languagesResult.getText(), "Frederick Phillips Brooks, Jr.");
+
+        WebElement heroResult = webDriver.findElement(By.xpath("//select[@name='languages']/following-sibling::label"));
+        Assert.assertEquals(heroResult.getText(), "Java, C++, Pascal");
+
+        WebElement linkReturnFromSelect = webDriver.findElement(By.xpath("//a[.='Great! Return to menu']"));
+        Assert.assertEquals(linkReturnFromSelect.getText(), "Great! Return to menu");
+        linkReturnFromSelect.click();
+
+        webDriver.findElement(By.id("form")).click();
+        fillField("First Name:", "Ivan");
+        fillField("Last Name:", "Petrov");
+        fillField("Email:", "ivan.petrov@yandex.ru");
+        fillField("Address:", "Moscow, The Red Square, 1");
+
+        webDriver.findElement(By.xpath("//input[@name='sex']")).click();
+
+        webDriver.findElement(By.tagName("textarea")).sendKeys("London is the capital of Great Britain");
+
+        //webDriver.findElement(By.xpath("//input[@type='file']")).click();
+        //выбрать файл
+
+        webDriver.findElement(By.xpath("//input[@type='submit']")).click();
+        //4. Проверить, что появилась ссылка с текстом “Great! Return to menu” и нажать на неё.
+*/
+        webDriver.findElement(By.id("iframe")).click();
+        webDriver.switchTo().frame("code-frame");
+        String codeText = webDriver.findElement(By.id("code")).getText().substring(14);
+        webDriver.switchTo().defaultContent();
+        webDriver.findElement(By.name("code")).sendKeys(codeText);
+        webDriver.findElement(By.name("ok")).click();
+
+        WebElement linkReturnFromIFrame = webDriver.findElement(By.xpath("//a[.='Great! Return to menu']"));
+        Assert.assertEquals(linkReturnFromIFrame.getText(), "Great! Return to menu");
+        linkReturnFromIFrame.click();
+
+    }
+
+    @AfterMethod
+    public void closeDriver() {
+        //webDriver.quit();
+    }
+
+    public void fillField(String fieldTitle, String inputText){
+        WebElement input = webDriver.findElement(By.xpath("//label[.='" + fieldTitle + "']/following-sibling::input"));
+        input.sendKeys(inputText);
+    }
+
+}
