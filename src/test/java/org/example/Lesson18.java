ee40679bbca6cde8b3e9c47211a371b4cdae9891
Victor Untulius <untulius@gmail.com>
2020-03-30 18:29:50 +0300

    Lesson18 complete

--- src/test/java/org/example/Lesson18.java
+++ src/test/java/org/example/Lesson18.java
@@ -1,12 +1,8 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
@@ -15,10 +11,6 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
 /*
@@ -49,7 +41,7 @@ public class Lesson18 {
     @Test
     public void lesson18() {
         webDriver.get("https://savkk.github.io/selenium-practice/");
-        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        webDriver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
         webDriver.findElement(By.id("alerts")).click();
         webDriver.findElement(By.xpath("//button[.='Get password']")).click();
@@ -68,103 +60,49 @@ public class Lesson18 {
         Alert confirm = webDriver.switchTo().alert();
         confirm.accept();
 
-        //2. Написать негативный тест на первое задание.
+        //2. Написать негативный тест на первое задание:
+        webDriver.findElement(By.id("alerts")).click();
+        webDriver.findElement(By.xpath("//button[.='Get password']")).click();
+        Alert alertNegativeTest = webDriver.switchTo().alert();
+        alertNegativeTest.accept();
+        webDriver.findElement(By.xpath("//button[.='Enter password']")).click();
+        Alert promptNegativeTest = webDriver.switchTo().alert();
+        promptNegativeTest.sendKeys("noValidPassword1234");
+        promptNegativeTest.accept();
 
+        Assert.assertFalse(isElementPresent("//label[.='Great!']"));
+        Assert.assertFalse(isElementPresent("//button[.='Return to menu']"));
+
+        //3.
+        webDriver.navigate().back();
         webDriver.findElement(By.id("table")).click();
-
-/*
-        //Lesson16:
-        webDriver.findElement(By.id("button")).click();
-        webDriver.findElement(By.id("first")).click();
-
-        WebElement labelExcellent = webDriver.findElement(By.xpath("//label[.='Excellent!']"));
-        Assert.assertEquals(labelExcellent.getText(), "Excellent!");
-
-        WebElement button = webDriver.findElement(By.className("button-primary"));
-        Assert.assertEquals(button.getAttribute("value"), "Click me too!");
-        button.click();
-
-        WebElement link = webDriver.findElement(By.xpath("//a[.='Great! Return to menu']"));
-        Assert.assertEquals(link.getText(), "Great! Return to menu");
-        link.click();
-
-        webDriver.findElement(By.id("checkbox")).click();
-
-        WebElement checkBoxOne = webDriver.findElement(By.id("one"));
-        checkBoxOne.click();
-        webDriver.findElement(By.id("go")).click();
-        WebElement labelResult = webDriver.findElement(By.id("result"));
-        Assert.assertEquals(labelResult.getText(), checkBoxOne.getAttribute("value"));
-
-        WebElement radioBoxThree = webDriver.findElement(By.id("radio_three"));
-        radioBoxThree.click();
-        webDriver.findElement(By.id("radio_go")).click();
-        WebElement labelRadioResult = webDriver.findElement(By.id("radio_result"));
-        Assert.assertEquals(labelRadioResult.getText(), radioBoxThree.getAttribute("value"));
-
-        WebElement linkReturn = webDriver.findElement(By.xpath("//a[.='Great! Return to menu']"));
-        Assert.assertEquals(linkReturn.getText(), "Great! Return to menu");
-        linkReturn.click();
-
-
-        //Lesson17:
-        webDriver.findElement(By.id("select")).click();
-        WebElement heroElement = webDriver.findElement(By.name("hero"));
-        Select heroSelect = new Select(heroElement);
-        heroSelect.selectByVisibleText("Frederick Phillips Brooks, Jr.");
-
-        WebElement languagesElement = webDriver.findElement(By.name("languages"));
-        Select languagesSelect = new Select(languagesElement);
-        languagesSelect.selectByValue("Java");
-        languagesSelect.selectByValue("C++");
-        languagesSelect.selectByValue("Pascal");
-
-        webDriver.findElement(By.id("go")).click();
-        WebElement languagesResult = webDriver.findElement(By.xpath("//select[@name='hero']/following-sibling::label"));
-        Assert.assertEquals(languagesResult.getText(), "Frederick Phillips Brooks, Jr.");
-
-        WebElement heroResult = webDriver.findElement(By.xpath("//select[@name='languages']/following-sibling::label"));
-        Assert.assertEquals(heroResult.getText(), "Java, C++, Pascal");
-
-        WebElement linkReturnFromSelect = webDriver.findElement(By.xpath("//a[.='Great! Return to menu']"));
-        Assert.assertEquals(linkReturnFromSelect.getText(), "Great! Return to menu");
-        linkReturnFromSelect.click();
-
-        webDriver.findElement(By.id("form")).click();
-        fillField("First Name:", "Ivan");
-        fillField("Last Name:", "Petrov");
-        fillField("Email:", "ivan.petrov@yandex.ru");
-        fillField("Address:", "Moscow, The Red Square, 1");
-
-        webDriver.findElement(By.xpath("//input[@name='sex']")).click();
-
-        webDriver.findElement(By.tagName("textarea")).sendKeys("London is the capital of Great Britain");
-
-        Path testFile1 = Files.createFile(Paths.get("src\\HelloWorld.txt"));
-        webDriver.findElement(By.xpath("//input[@type='file']")).sendKeys(testFile1.toAbsolutePath().toString());
-        Files.delete(testFile1);
-
-        webDriver.findElement(By.xpath("//input[@type='submit']")).click();
-        WebElement linkReturnFromForm = webDriver.findElement(By.xpath("//a[.='Great! Return to menu']"));
-        Assert.assertEquals(linkReturnFromForm.getText(), "Great! Return to menu");
-        linkReturnFromForm.click();
-
-        webDriver.findElement(By.id("iframe")).click();
-        webDriver.switchTo().frame("code-frame");
-        String codeText = webDriver.findElement(By.id("code")).getText().substring(14);
-        webDriver.switchTo().defaultContent();
-        webDriver.findElement(By.name("code")).sendKeys(codeText);
-        webDriver.findElement(By.name("ok")).click();
-
-        WebElement linkReturnFromIFrame = webDriver.findElement(By.xpath("//a[.='Great! Return to menu']"));
-        Assert.assertEquals(linkReturnFromIFrame.getText(), "Great! Return to menu");
-        linkReturnFromIFrame.click();
- */
+        webDriver.findElement(By.xpath("//table/tbody/tr[3]/td/input")).click();
+        webDriver.findElement(By.xpath("//input[@value='Delete']")).click();
+        fillField("Company", "Yandex");
+        fillField("Contact", "Moscow");
+        fillField("Country", "Russia");
+        webDriver.findElement(By.xpath("//input[@value='Add']")).click();
+        fillField("Company", "Google");
+        fillField("Contact", "Moscow");
+        fillField("Country", "1600 Amphitheatre Parkway, Mountain View, CA 94043");
+        webDriver.findElement(By.xpath("//input[@value='Add']")).click();
+        WebElement linkReturnFromTable = webDriver.findElement(By.xpath("//a[.='Great! Return to menu']"));
+        Assert.assertEquals(linkReturnFromTable.getText(), "Great! Return to menu");
+        linkReturnFromTable.click();
     }
 
     @AfterMethod
     public void closeDriver() {
-        //webDriver.quit();
+        webDriver.quit();
+    }
+
+    public boolean isElementPresent(String locator) {
+        try {
+            webDriver.findElement(By.xpath(locator));
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
 
     public void fillField(String fieldTitle, String inputText) {
